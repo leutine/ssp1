d33564c95e620ae6bffeef3c4d337cc33c54f4a4
Eugene Leutin <leutin.e@gmail.com>
2019-03-26 23:24:32 +0300

    Add Triangle class

--- main/src/main/Triangle.java
+++ main/src/main/Triangle.java
@@ -3,20 +3,47 @@ package main;
 import java.util.Arrays;
 
 class Triangle {
-    static int[][] generate(int lowerb, int upperb) {
-        int a[] = {Utils.generate_int(lowerb, upperb), Utils.generate_int(lowerb, upperb)};
-        int b[] = {Utils.generate_int(lowerb, upperb), Utils.generate_int(lowerb, upperb)};
-        int c[] = {Utils.generate_int(lowerb, upperb), Utils.generate_int(lowerb, upperb)};
+    private Point A;
+    private Point B;
+    private Point C;
 
-        return new int[][] {a, b, c};
+    Triangle(int[] borders) {
+        A = new Point(borders);
+        B = new Point(borders);
+        C = new Point(borders);
     }
 
-    static void print(int[][] points) {
-        System.out.printf("Triangle points: \nA: %s\nB: %s\nC: %s",
-                Arrays.toString(points[0]),
-                Arrays.toString(points[1]),
-                Arrays.toString(points[2]));
-        System.out.println();
-        System.out.println();
+    Triangle(Point A_point, Point B_point, Point C_point) {
+        A = A_point;
+        B = B_point;
+        C = C_point;
     }
+
+    Point get_A() {
+        return this.A;
+    }
+
+    Point get_B() {
+        return this.B;
+    }
+
+    Point get_C() {
+        return this.C;
+    }
+
+    void print() {
+        System.out.printf("Triangle points' coordinates [X, Y]: \nA: %s\nB: %s\nC: %s",
+                Arrays.toString(A.get_coordinates()),
+                Arrays.toString(B.get_coordinates()),
+                Arrays.toString(C.get_coordinates()));
+        System.out.print("\n\n");
+    }
+
+//    static int[][] generate(int lowerb, int upperb) {
+//        int a[] = {Utils.generate_int(lowerb, upperb), Utils.generate_int(lowerb, upperb)};
+//        int b[] = {Utils.generate_int(lowerb, upperb), Utils.generate_int(lowerb, upperb)};
+//        int c[] = {Utils.generate_int(lowerb, upperb), Utils.generate_int(lowerb, upperb)};
+//
+//        return new int[][] {a, b, c};
+//    }
 }
