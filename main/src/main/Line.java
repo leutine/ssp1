ff990c5a102f5c650a2f6b7406a844c9c78dde68
Eugene Leutin <leutin.e@gmail.com>
2019-03-26 23:24:11 +0300

    Add Line class

--- main/src/main/Line.java
+++ main/src/main/Line.java
@@ -1,4 +1,29 @@
 package main;
 
 public class Line {
+    private int k;
+    private int b;
+
+    Line(int[] borders) {
+        k = Utils.generate_int(borders[0], borders[1]);
+        b = Utils.generate_int(borders[0], borders[1]);
+    }
+
+    Line(int k_coefficient, int b_coefficient) {
+        k = k_coefficient;
+        b = b_coefficient;
+    }
+
+    void print() {
+        System.out.printf("Line equation: y = %dx%+d", k, b);
+        System.out.print("\n\n");
+    }
+
+    public int get_k() {
+        return this.k;
+    }
+
+    public int get_b() {
+        return this.b;
+    }
 }
