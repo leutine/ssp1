0811b912f039a26b842c465f84529b39b49f8036
Eugene Leutin <leutin.e@gmail.com>
2019-03-26 23:23:59 +0300

    Add Square class

--- main/src/main/Square.java
+++ main/src/main/Square.java
@@ -3,22 +3,56 @@ package main;
 import java.util.Arrays;
 
 class Square {
-    static int[][] generate(int lowerb, int upperb) {
-        int a[] = {Utils.generate_int(lowerb, upperb), Utils.generate_int(lowerb, upperb)};
-        int b[] = {Utils.generate_int(lowerb, a[0]), a[1]};
-        int c[] = {a[0], Utils.generate_int(lowerb, a[1])};
-        int d[] = {b[0], c[1]};
+    private Point A;
+    private Point B;
+    private Point C;
+    private Point D;
 
-        return new int[][] {a, b, c, d};
+    Square(int[] borders) {
+        borders[0]++;
+        int side;
+        do {
+            A = new Point(borders);
+            B = new Point(Utils.generate_int(borders[0], A.get_x()), A.get_y());
+            side = A.get_x() - B.get_x();
+            C = new Point(A.get_x(), A.get_y() - side);
+            D = new Point(B.get_x(), C.get_y());
+        } while (!C.IsWithinBorders(borders));
     }
 
-    static void print(int[][] points) {
-        System.out.printf("Square points: \nA: %s\nB: %s\nC: %s\nD: %s",
-                Arrays.toString(points[0]),
-                Arrays.toString(points[1]),
-                Arrays.toString(points[2]),
-                Arrays.toString(points[3]));
-        System.out.println();
-        System.out.println();
+    Square(Point A_point, Point B_point, Point C_point, Point D_point) {
+        A = A_point;
+        B = B_point;
+        C = C_point;
+        D = D_point;
+    }
+
+    Point get_A() {
+        return this.A;
+    }
+
+    Point get_B() {
+        return this.B;
+    }
+
+    Point get_C() {
+        return this.C;
+    }
+
+    Point get_D() {
+        return this.D;
+    }
+
+    Point[] get_points() {
+        return new Point[]{this.A, this.B, this.C, this.D};
+    }
+
+    void print() {
+        System.out.printf("Square points' coordinates [X, Y]: \nA: %s\nB: %s\nC: %s\nD: %s",
+                Arrays.toString(A.get_coordinates()),
+                Arrays.toString(B.get_coordinates()),
+                Arrays.toString(C.get_coordinates()),
+                Arrays.toString(D.get_coordinates()));
+        System.out.print("\n\n");
     }
 }
