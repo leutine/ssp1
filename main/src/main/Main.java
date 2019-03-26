3cd22825d205a264b7a1af50e8f23b30b7176820
Eugene Leutin <leutin.e@gmail.com>
2019-03-26 23:25:15 +0300

    Add Main class, Add coords' input

--- main/src/main/Main.java
+++ main/src/main/Main.java
@@ -1,19 +1,45 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     private static final int BORDERS[] = {-100, 100};
-    private static final int UPPER_BORDER = BORDERS[1];
     private static final int LOWER_BORDER = BORDERS[0];
+    private static final int UPPER_BORDER = BORDERS[1];
 
     public static void main(String[] args) {
-        int point[] = Point.generate(LOWER_BORDER, UPPER_BORDER);
-        int[][] square = Square.generate(LOWER_BORDER, UPPER_BORDER);
-        int[][] triangle = Triangle.generate(LOWER_BORDER, UPPER_BORDER);
+        int[] coordinates = {-20, 0};
 
+//        System.out.println("Enter coordinates X and Y each in new line:");
+//        for (int i:coordinates) {
+//            Scanner input = new Scanner(System.in);
+//            coordinates[i] = input.nextInt();
+//        }
+        Point A = new Point(10, 10);
+        Point B = new Point(0, 10);
+        Point C = new Point(10, 0);
+        Point D = new Point(0, 0);
 
-        Point.print(point);
-        Square.print(square);
-        Triangle.print(triangle);
+        Point point = new Point(coordinates[0], coordinates[1]);
+        Triangle triangle = new Triangle(BORDERS);
+//        Square square = new Square(BORDERS);
+        Square square = new Square(A, B, C, D);
+//        Line line = new Line(BORDERS);
+        Line line = new Line(1, 10);
+//        Circle circle = new Circle(BORDERS);
+        Circle circle = new Circle(D, 10);
+
+        System.out.println("Point's coordinates within borders set: " + point.IsWithinBorders(BORDERS) + "\n");
+        point.print();
+        triangle.print();
+        square.print();
+        line.print();
+        circle.print();
+
+        point.RelationWith(triangle);
+        point.RelationWith(square);
+        point.RelationWith(circle);
+        point.RelationWith(line);
     }
 }
 
